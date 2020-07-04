24deaee557f231e8fb499f422243b322f2169320
Ravi5ingh <ravisingh1203@hotmail.com>
2020-07-04 15:54:48 +0100

    move website stuff to 'website' folder

---
+++ website/app.py
@@ -0,0 +1,16 @@
+import flask as fl
+
+app = fl.Flask(__name__)
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+
+    return fl.render_template('master.html')
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+if __name__ == '__main__':
+    main()
