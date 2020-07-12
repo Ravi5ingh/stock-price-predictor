8a2f866e26117497a2b4e143c1f35cfcc9cdddf5
Ravi5ingh <ravisingh1203@hotmail.com>
2020-07-12 15:51:59 +0100

    change port and use config

--- website/app.py
+++ website/app.py
@@ -1,4 +1,5 @@
 import flask as fl
+import utility.config as cf
 
 app = fl.Flask(__name__)
 
@@ -9,7 +10,7 @@ def index():
     return fl.render_template('master.html')
 
 def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+    app.run(host='0.0.0.0', port=3002, debug=cf.is_app_debug())
 
 if __name__ == '__main__':
     main()
