2b791760fc8a4a514ff7f1685c592aae17264b35
Ravi5ingh <ravisingh1203@hotmail.com>
2020-08-01 14:56:09 +0100

    rename app file

--- website/run.py
+++ website/run.py
@@ -16,7 +16,7 @@ def index():
     stock_symbol = rq.args.get('symbol')
     stock_symbol = stock_symbol if stock_symbol is not None else 'MSFT'
 
-    # Get the daily closing prices for the given stock (default to 1 year for now)
+    # Get visualization data for the charts we want to display
     graphs = [
         vz.get_stock_ts(stock_symbol, period='1Y'),
         vz.get_last_day_predictions(stock_symbol, period='1mo')
