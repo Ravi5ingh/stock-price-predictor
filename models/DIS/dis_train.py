9c50e6d66974e772768a22dfd8db930948084c3d
Ravi5ingh <ravisingh1203@hotmail.com>
2020-08-07 12:24:31 +0100

    setup website to viz any trained models

--- models/DIS/dis_train.py
+++ models/DIS/dis_train.py
@@ -3,12 +3,16 @@ import models.lstm_uv as ls
 import pandas as pd
 
 # Get and save the data
-dis = yf.Ticker('DIS').history(period='10Y').reset_index()
-dis[0:2000].to_csv('dis_train.csv', index=False)
-dis[2000:].to_csv('dis_test.csv', index=False)
+# dis = yf.Ticker('DIS').history(period='10Y').reset_index()
+# dis[0:2000].to_csv('dis_train.csv', index=False)
+# dis[2000:].to_csv('dis_test.csv', index=False)
 
-ibm_train = pd.read_csv('dis_train.csv')
+dis_train = pd.read_csv('dis_train.csv')
 
 model = ls.LongShortTermMemoryModel()
 
-model.fit_transform(ibm_train, 'lstm_model', 'scaler.pkl')
+# model.fit_transform(dis_train, 'lstm_model', 'scaler.pkl')
+
+chart_df, rmse = model.predict(dis_train, pd.read_csv('dis_test.csv'), 'lstm_model', 'scaler.pkl')
+
+chart_df.to_csv('chart_df.csv', index=False)
